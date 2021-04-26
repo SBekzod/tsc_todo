a0eb2154dba3714fb00ce317bb6274934699d19c
Martin <bekzodsanakulov89@gmail.com>
2021-04-26 11:58:31 +0900

    feat: forming index.ts to execute the logic

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoCollection_1 = require("./todoCollection");
+let todoList = [
+    new todoItem_1.TodoItem(1, "Buy Flowers"),
+    new todoItem_1.TodoItem(2, "Get Shoes"),
+    new todoItem_1.TodoItem(3, "Collect tickets"),
+    new todoItem_1.TodoItem(4, "Call to Jack"),
+];
+let collection = new todoCollection_1.TodoCollection('Martin', todoList);
 console.clear();
-console.log(`Today's Todo List is here!`);
+console.log(`${collection.userName}'s Todo List`);
+let newId = collection.addTodo('Go to run');
+let todoItem = collection.getTodoById(newId);
+console.log(JSON.stringify(todoItem));
+console.log("\n here is the full list: ");
+console.log(JSON.stringify(todoList));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-console.clear()
-console.log(`Today's Todo List is here!`)
+import {TodoItem} from "./todoItem";
+import {TodoCollection} from "./todoCollection";
+
+let todoList = [
+    new TodoItem(1, "Buy Flowers"),
+    new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect tickets"),
+    new TodoItem(4, "Call to Jack"),
+];
+
+let collection = new TodoCollection('Martin', todoList);
+
+console.clear();
+console.log(`${collection.userName}'s Todo List`);
+
+let newId = collection.addTodo('Go to run');
+let todoItem = collection.getTodoById(newId);
+console.log(JSON.stringify(todoItem));
+console.log("\n here is the full list: ");
+console.log(JSON.stringify(todoList));

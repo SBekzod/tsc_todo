38561325237ddcdffccdca2573073c010031c632
Martin <bekzodsanakulov89@gmail.com>
2021-04-26 16:46:47 +0900

    feat: using inquirer package to prompt users

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const todoItem_1 = require("./todoItem");
 const todoCollection_1 = require("./todoCollection");
+const inquirer = require("inquirer");
 let todoList = [
     new todoItem_1.TodoItem(1, "Buy Flowers"),
     new todoItem_1.TodoItem(2, "Get Shoes"),
@@ -10,10 +11,35 @@ let todoList = [
 ];
 let collection = new todoCollection_1.TodoCollection('Martin', todoList);
 collection.addTodo('Play basketball');
-console.clear();
-console.log(`${collection.userName}'s Todo List: `);
-let itemCountInfo = collection.getItemCounts();
-console.log(`The total tasks: ${itemCountInfo.total} and incomplete tasks: ${itemCountInfo.incomplete}`);
-collection.getTodoItems(true).forEach(function (item) {
-    item.printDetails();
-});
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List: `);
+    let itemCountInfo = collection.getItemCounts();
+    console.log(`The total tasks: ${itemCountInfo.total} and incomplete tasks: ${itemCountInfo.incomplete}`);
+    collection.getTodoItems(true).forEach(function (item) {
+        item.printDetails();
+    });
+}
+var Commands;
+(function (Commands) {
+    Commands["Wait"] = "Wait";
+    Commands["Goon"] = "Goon";
+    Commands["Quit"] = "Quit";
+    Commands["Other"] = "Other";
+})(Commands || (Commands = {}));
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose options",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers['command'] !== Commands.Quit) {
+            promptUser();
+        }
+    });
+}
+promptUser();
+console.log('Type:', typeof (Object.values(Commands)));
+console.log('Value:', Object.values(Commands));
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,48 @@
 import {TodoItem} from "./todoItem";
 import {TodoCollection} from "./todoCollection";
+import * as inquirer from "inquirer";
 
-let todoList = [
+let todoList: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"),
     new TodoItem(2, "Get Shoes"),
     new TodoItem(3, "Collect tickets"),
     new TodoItem(4, "Call to Jack", true),
 ];
 
-let collection = new TodoCollection('Martin', todoList);
+let collection: TodoCollection = new TodoCollection('Martin', todoList);
 collection.addTodo('Play basketball');
 
-console.clear();
-console.log(`${collection.userName}'s Todo List: `);
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List: `);
+    let itemCountInfo = collection.getItemCounts();
+    console.log(`The total tasks: ${itemCountInfo.total} and incomplete tasks: ${itemCountInfo.incomplete}`);
+    collection.getTodoItems(true).forEach(function (item) {
+        item.printDetails();
+    });
+}
 
-let itemCountInfo = collection.getItemCounts();
-console.log(`The total tasks: ${itemCountInfo.total} and incomplete tasks: ${itemCountInfo.incomplete}`);
+enum Commands {
+    Wait = "Wait",
+    Goon = "Goon",
+    Quit = "Quit",
+    Other = "Other"
+}
 
-collection.getTodoItems(true).forEach(function (item) {
-    item.printDetails();
-});
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose options",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers['command'] !== Commands.Quit) {
+            promptUser();
+        }
+    });
+}
+
+promptUser();
+
 
